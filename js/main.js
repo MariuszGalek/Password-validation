e2cad89b64fa7f1948a4ffcb0efd5fa0c7bf3d86
Mariusz Galek <MariuszGalek@users.noreply.github.com>
2020-04-15 14:51:45 +0200

    Add validation functions

--- js/main.js
+++ js/main.js
@@ -4,3 +4,25 @@ const letters = /[a-z]/i;
 const numbers = /[0-9]/;
 const special = /[!@#$%^&*()]/;
 const minLength = 10;
+
+const checkPassword = () => {
+    if (password.value.length >= minLength && password.value.match(numbers) && password.value.match(letters) && password.value.match(special)) {
+        info.innerHTML = 'Your password is very strong!';
+        info.style.color = 'lime'
+    } else if (password.value.length >= minLength && password.value.match(numbers) && password.value.match(letters)) {
+        info.innerHTML = 'Your password is ok.';
+        info.style.color = 'gold'
+    } else {
+        info.innerHTML = 'Your password is weak.';
+        info.style.color = 'red'
+    };
+};
+
+password.addEventListener('keyup', function(){
+    if(password.value !== '') {
+        checkPassword();
+    } else {
+        info.innerHTML = 'You must enter a password...';
+        info.style.color = null;
+    };
+})
