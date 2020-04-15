c911a8ab3790e8affcfadcd2d002380bcd9d3c73
Mariusz Galek <MariuszGalek@users.noreply.github.com>
2020-04-15 12:01:03 +0200

    Add variables

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const password = document.querySelector('#password');
+const info = document.querySelector('.passInfo');
+const letters = /[a-z]/i;
+const numbers = /[0-9]/;
+const special = /[!@#$%^&*()]/;
+const minLength = 10;
